418bc8d09e4d00b8a5ffb3eaa72f4c6900eb6689
javyer <70788090+javyer12@users.noreply.github.com>
2022-06-16 21:02:10 -0600

    components style

--- src/Balls.js
+++ src/Balls.js
@@ -1,17 +1,19 @@
 //ROLEX
-const time = document.getElementById("timer");
+const timer = new Date();
 
 function Timer() {
-    const timer = new Date();
-    time.textContent = timer;
+  const time = document.getElementById("timer");
+  const timer = new Date().toLocaleTimeString();
+  time.innerHTML = timer;
 }
 Timer();
+console.log(timer);
 
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
 
 // function to generate random number
 
@@ -23,98 +25,98 @@ function random(min, max) {
 // function to generate random color
 
 function randomRGB() {
-  return `rgb(${random(0, 0)},${random(15, 155)},${random(190, 255)})`;
+  return `rgb(${random(0, 0)},${random(179, 150)},${random(155, 185)})`;
 }
 
 class Ball {
-    constructor (x,y, velX, velY, color, size){
-        this.x = x;
-        this.y = y;
-        this.velX = velX;
-        this.velY = velY;
-        this.color = color;
-        this.size = size;
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
 
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 1.5 * Math.PI);
+    ctx.fill();
+  }
+
+  update() {
+    if (this.x + this.size >= width) {
+      this.velX = -this.velX;
     }
 
-    draw() {
-        ctx.beginPath();
-        ctx.fillStyle = this.color;
-        ctx.arc(this.x, this.y, this.size, 0,1.5 * Math.PI);
-        ctx.fill();
+    if (this.x - this.size <= 0) {
+      this.velX = -this.velX;
     }
 
-    update() {
-        if ((this.x + this.size) >= width) {
-           this.velX = -(this.velX);
-        }
-     
-        if ((this.x - this.size) <= 0) {
-           this.velX = -(this.velX);
-        }
-     
-        if ((this.y + this.size) >= height) {
-           this.velY = -(this.velY);
-        }
-     
-        if ((this.y - this.size) <= 0) {
-           this.velY = -(this.velY);
-        }
-     
-        this.x += this.velX;
-        this.y += this.velY;
-     }
+    if (this.y + this.size >= height) {
+      this.velY = -this.velY;
+    }
 
-     collisionDetect() {
-      for (const ball of balls) {
-         if (!(this === ball)) {
-            const dx = this.x - ball.x;
-            const dy = this.y - ball.y;
-            const distance = Math.sqrt(dx * dx + dy * dy);
-   
-            if (distance < this.size + ball.size) {
-              ball.color = this.color = randomRGB();
-            }
-         }
+    if (this.y - this.size <= 0) {
+      this.velY = -this.velY;
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (!(this === ball)) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
       }
-   }
+    }
+  }
 }
 
-const testBall = new Ball(100   , 100, 4, 8, 'red',8);
+const testBall = new Ball(100, 100, 4, 8, "red", 8);
 
-testBall.x
-testBall.size
-testBall.color
-testBall.draw()
+testBall.x;
+testBall.size;
+testBall.color;
+testBall.draw();
 
 const balls = [];
 
 while (balls.length < 35) {
-   const size = random(10,20);
-   const ball = new Ball(
-      // ball position always drawn at least one ball width
-      // away from the edge of the canvas, to avoid drawing errors
-      random(0 + size,width - size),
-      random(0 + size,height - size),
-      random(-7,7),
-      random(-7,7),
-      randomRGB(),
-      size
-   );
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
 
   balls.push(ball);
 }
 
-    function loop() {
-        ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
-        ctx.fillRect(0, 0, width, height);
-    
-        for (const ball of balls) {
-        ball.draw();
-        ball.update();
-        }
-    
-        requestAnimationFrame(loop);
-    }
+function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
 
-    loop();
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 
-const greeting = document.getElementById("greetings");
-greeting.addEventListener("mouseover",Greeting);
-greeting.addEventListener("keyup",Greetings);
+// const greeting = document.getElementById("greetings");
+// greeting.addEventListener("mouseover",Greeting);
+// greeting.addEventListener("keyup",Greetings);
 
-function Greeting() {
-    console.log("hii,welcome, this is the mouseover event")
-}
-const SAVED = [];
-function Greetings() {
-    const val= greeting.value;
-    console.log(val);
-    const txt = document.createElement("p");
-    txt.textContent = val;
-    if (val){
-        SAVED.push(val)
-    }
-    document.body.append(SAVED); 
-}
+// function Greeting() {
+//     console.log("hii,welcome, this is the mouseover event")
+// }
+// const SAVED = [];
+// function Greetings() {
+//     const val= greeting.value;
+//     console.log(val);
+//     const txt = document.createElement("p");
+//     txt.textContent = val;
+//     if (val){
+//         SAVED.push(val)
+//     }
+//     document.body.append(SAVED); 
+// }
 
 // const names = ["field", "campus", "az", "build"];
 
@@ -33,28 +33,7 @@ function Greetings() {
 //     // document.body.append(p)
 // }
 
-const genereText = [
-    "the asp net is a framework to develop with c# most known in microsoft world, but lastest many people is turning into it",
-    "the basic concepts of js you can find them on my own site",
-    "Asynchronous development is a new technique to make differentes things in the same time",
-]
 
-// function text() {
-
-//     const campus = document.getElementById("insert_p");
-//     for( i in genereText){
-//         console.log(genereText[i])
-//         const p = document.createElement("p");
-//         p.genereText[i];
-//         campus.append(p)
-//         console.log(campus)
-        
-//     }
-// }
-
-// text();
-
-//tomar el curso de manipulacion de arrays
 
 function updateText(e){
     let parrafo = document.querySelector("#parrafo");
